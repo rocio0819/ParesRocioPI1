1119b27a71e271368e0dd6ac09c74437324d7e0d
rocio0819 <111366772+rocio0819@users.noreply.github.com>
2022-09-05 09:51:42 -0300

    Create MiArchivo.js

---
+++ MiArchivo.js
@@ -0,0 +1,10 @@
+<! DOCTYPE HTML>
+<html>
+<body>
+<p> Antes de la secuencia de comandos ... </p>
+<script>
+alert('¡Hola mundo!');
+</script>
+<p> ... Después de la secuencia de comandos. </p>
+</body>
+</html>
